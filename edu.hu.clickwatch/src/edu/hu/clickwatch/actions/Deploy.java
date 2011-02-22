6c929de94308d2050dde457d42b5e1f53c56acef
U-bean7x\zubow <zubow@bean7x.(none)>
2011-02-22 19:44:02 +0100

    cleanup

--- edu.hu.clickwatch/src/edu/hu/clickwatch/actions/Deploy.java
+++ edu.hu.clickwatch/src/edu/hu/clickwatch/actions/Deploy.java
@@ -20,6 +20,8 @@ import java.util.List;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
@@ -33,6 +35,8 @@ import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 
@@ -41,24 +45,196 @@ import com.jcraft.jsch.*;
 
 import edu.hu.clickwatch.ClickWatchPluginActivator;
 import edu.hu.clickwatch.GuiceModule;
+import edu.hu.clickwatch.XmlUtil;
+import edu.hu.clickwatch.actions.Execute.WorkerThread;
 import edu.hu.clickwatch.model.AbstractNodeConnection;
 import edu.hu.clickwatch.model.ClickControlNodeConnection;
 import edu.hu.clickwatch.model.MultiNode;
 import edu.hu.clickwatch.model.MultiNodeNodeConnection;
 import edu.hu.clickwatch.model.Node;
 import edu.hu.clickwatch.util.SshConnectionFactory;
+import edu.hu.clickwatch.views.ResultView;
 
 /**
- * Deploy a new Click configuration to remote nodes via ssh.
+ * Deploy a new Click configuration to remote nodes via ssh in parallel.
  * @author zubow
  */
 public class Deploy implements IObjectActionDelegate {
 
-	public final static String SSH_USER = "root";
-	
 	private Shell shell;
 	private IEditorPart editor = null;
 	private List<Node> node_lst;
+	private EObject currentResult = null;
+	
+	// synchronized counter
+	public class Counter {
+		private int count;
+		public synchronized void inc() {
+			count++;
+		}
+		public synchronized int get() {
+			return count;
+		}
+	}
+	
+	/**
+	 * This threads is responsible for observing worker/run threads for their progress. 
+	 * Based on that the progress dialog is updated.
+	 */
+	public class ObserverThread extends Thread {
+		private WorkerStatus[] observedThreads;
+		private IProgressMonitor monitor;
+		private Counter counter;
+		private String title;
+		
+		public ObserverThread(WorkerStatus[] observedThreads, IProgressMonitor monitor, Counter counter, String title) {
+			this.observedThreads = observedThreads;
+			this.monitor = monitor;
+			this.counter = counter;
+			this.title = title;
+		}
+
+		public void run() {
+			while (true) {
+				int count_before = counter.get();
+				int running_cnt = 0;
+				for (WorkerStatus worker : observedThreads) {
+					if (!worker.hasFinished()) {
+						running_cnt++;
+					}
+				}
+				if (running_cnt == 0) {
+					break; // finished
+				}
+
+				String monName = title + " waiting: " + (observedThreads.length-running_cnt) + " / " + (observedThreads.length);
+				
+				if (running_cnt <= 3) {
+					StringBuffer runThr = new StringBuffer();
+					for (WorkerStatus worker : observedThreads) {
+						if (!worker.hasFinished()) {
+							runThr.append(worker.getNodeName()).append(", ");
+						}
+					}
+					monName += " (Outstanding: " + runThr.toString() + ")";
+				}
+				
+				monitor.setTaskName(monName);
+				try {
+					sleep(50);
+				} catch (InterruptedException e) { }
+				int progress = counter.get() - count_before;
+				monitor.worked(progress);
+				if (monitor.isCanceled()) {
+					monitor.done();
+					throw new RuntimeException("User canceled operation!");
+				}
+			}
+		}
+	}
+	
+	public interface WorkerStatus {
+		public boolean hasFinished();
+		public String getNodeName();
+	}
+	
+	/**
+	 * Executing deployment in parallel - clean-up, copy, unpack
+	 */
+	public class PrepareWorkerThread extends Thread implements WorkerStatus {
+		
+		public String iNodeAddr;
+		public String lfile;
+		public String md5cs;
+		public String result;
+		public Exception exception;
+		private Counter counter;
+		private boolean finished;
+		
+		public PrepareWorkerThread(String iNodeAddr, String lfile, String md5cs, Counter counter) {
+			this.iNodeAddr = iNodeAddr;
+			this.lfile = lfile;
+			this.md5cs = md5cs;
+			this.counter = counter;
+			finished = false;
+		}
+		
+		public void run() {
+			try {
+				// remote deploy
+				System.out.println("deploying on node " + iNodeAddr + " called.");
+				result = DeploymentHelper.getInstance().deployRemote(iNodeAddr, lfile, md5cs, shell, false);
+			} catch (Exception e) {
+				System.err.println("ErrorMsg:" + e.getMessage());
+				exception = e;			
+			} finally {
+				counter.inc();
+				finished = true;
+			}
+		}
+		
+		public boolean failed() {
+			return (exception != null);
+		}
+
+		@Override
+		public boolean hasFinished() {
+			return finished;
+		}
+		
+		public String getNodeName() {
+			return iNodeAddr;
+		}
+	}	
+	
+	/**
+	 * Start nodes in parallel
+	 */
+	public class RunWorkerThread extends Thread implements WorkerStatus {
+		
+		public String iNodeAddr;
+		public PrepareWorkerThread prepareThr;
+		public String result;
+		public Exception exception;
+		private Counter counter;
+		private boolean finished;
+		
+		public RunWorkerThread(String iNodeAddr, PrepareWorkerThread prepareThr, Counter counter) {
+			this.iNodeAddr = iNodeAddr;
+			this.prepareThr = prepareThr;
+			this.counter = counter;
+			finished = false;
+		}
+		
+		public void run() {
+			// start router only if preparation phase was successful
+			if (!prepareThr.failed()) {
+				System.out.println("starting on node " + iNodeAddr + " called.");
+				try {
+					result = DeploymentHelper.getInstance().startRemote(iNodeAddr, shell, false);
+				} catch (Exception e) {
+					System.err.println("ErrorMsg:" + e.getMessage());
+					exception = e;			
+				}
+			} else {
+				// do not start failed nodes
+			}
+			counter.inc();
+			finished = true;
+		}
+		
+		public boolean failed() {
+			return (exception != null);
+		}
+		
+		@Override
+		public boolean hasFinished() {
+			return finished;
+		}		
+		public String getNodeName() {
+			return iNodeAddr;
+		}
+	}	
 
 	/**
 	 * Constructor for Action1.
@@ -92,79 +268,185 @@ public class Deploy implements IObjectActionDelegate {
         fd.setText("Open");
         String[] filterExt = { "*.bz2", "*.tar", "*.gz", "*.zip", "*.dat", "*.*" };
         fd.setFilterExtensions(filterExt);
-        String lfile = fd.open();
+        final String lfile = fd.open();
         
         if (lfile == null) {
         	return;
         }
 
-        //
-        // Step 0: calc MD5 checksum
-        String md5cs = "";
-        FileInputStream fis;
+		//  create n parallel execution threads
+		final PrepareWorkerThread[] prepareWorkerThreads = new PrepareWorkerThread[node_lst.size()];
+		//  create n parallel execution threads
+		final RunWorkerThread[] runWorkerThreads = new RunWorkerThread[node_lst.size()];
+
 		try {
-			fis = new FileInputStream( new File(lfile) );
-	        md5cs = org.apache.commons.codec.digest.DigestUtils.md5Hex( fis );
-		} catch (Exception e1) {
-			System.err.println("ErrorMsg:" + e1.getMessage());
-			MessageDialog.openError(editor.getSite().getShell(), "Clickwatch Error", "ErrorMsg:" + e1.getMessage());
-		}
-        
-		// show log files only for single node deployment
-		boolean show_log = (node_lst.size() == 1) ? true : false;
-		
-        Iterator<Node> node_it = node_lst.iterator();
-        List<Boolean> mask = new ArrayList<Boolean>();
-		//
-        // Step 1: prepare, copy and unpack router conf
-		while (node_it.hasNext()) {
-			Node node = node_it.next();
+	        ProgressMonitorDialog dialog = new ProgressMonitorDialog(shell);
+	        dialog.run(true, true, new IRunnableWithProgress() {
+	            public void run(IProgressMonitor monitor) {
+		        	try {
+		        		int numtasks = 1 + 2 * node_lst.size();
+		        		Counter counter = new Counter();
+		        		monitor.beginTask("Remote deployment", numtasks);
 
-			// disconnect if connected
-			if (node.getConnection() != null) {
-				AbstractNodeConnection oldConnection = (AbstractNodeConnection)node.getConnection();
-				node.setConnection(null);
-				oldConnection.disconnect();
-			}
+				        //
+				        // Step 0: calc MD5 checksum
+						monitor.setTaskName("MD5 calc ...");
+				        String md5cs = "";
+				        FileInputStream fis;
+						try {
+							fis = new FileInputStream( new File(lfile) );
+					        md5cs = org.apache.commons.codec.digest.DigestUtils.md5Hex( fis );
+						} catch (Exception e1) {
+							System.err.println("ErrorMsg:" + e1.getMessage());
+							MessageDialog.openError(editor.getSite().getShell(), "Clickwatch Error", "ErrorMsg:" + e1.getMessage());
+						}
+						counter.inc();
+						monitor.worked(1);
+						
+						monitor.setTaskName("Prepare deployment ...");
+						final ObserverThread observer = new ObserverThread(prepareWorkerThreads, monitor, counter, "Prepare deployment ... ");
+						
+				        //
+				        // Step 1: prepare, copy and unpack router conf in parallel
+						for (int idx=0; idx<node_lst.size(); idx++) {
+							final Node node = node_lst.get(idx);
+				
+							// disconnect if connected
+							if (node.getConnection() != null) {
+								AbstractNodeConnection oldConnection = (AbstractNodeConnection)node.getConnection();
+								node.setConnection(null);
+								oldConnection.disconnect();
+							}
+							
+							// do it in parallel
+							prepareWorkerThreads[idx] = new PrepareWorkerThread(node.getINetAdress(), lfile, md5cs, counter);
+							prepareWorkerThreads[idx].start();
+						}
+						observer.start();
+						
+						// sync point: wait until all worker threads are finished
+						for (int i=0; i<prepareWorkerThreads.length; i++) {
+							prepareWorkerThreads[i].join();
+						}
+						observer.join();
+						
+						//monitor.worked(counter.get());
+						monitor.setTaskName("Start router conf ...");
+						
+						final ObserverThread observer2 = new ObserverThread(runWorkerThreads, monitor, counter, "Start router conf ...");
+						//
+				        // Step 2: start router in parallel
+						for (int idx=0; idx<node_lst.size(); idx++) {
+							final Node node = node_lst.get(idx);
+				
+							// do it in parallel
+							runWorkerThreads[idx] = new RunWorkerThread(node.getINetAdress(), prepareWorkerThreads[idx], counter);
+							runWorkerThreads[idx].start();
+						}
+						observer2.start();
+						
+						// sync point: wait until all worker threads are finished
+						for (int i=0; i<runWorkerThreads.length; i++) {
+							runWorkerThreads[i].join();
+						}
+						observer2.join();
+						
+						//monitor.worked(counter.get());
+		        	} catch(Exception e) {
+		    			System.err.println("Exception: " + e.getMessage());
+		        		MessageDialog.openError(shell, "Clickwatch Error", "Exception: " + e.getMessage());	
+		        	} finally {
+		        		monitor.done();
+		        	}
+	            }
+	        });
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+    		MessageDialog.openError(shell, "Clickwatch Error", "Exception: " + e.getMessage());	
+		}
+
+		//
+		// analyze results
+        final List<String> prepResults = new ArrayList<String>();
+        final List<String> runResults = new ArrayList<String>();
+        final List<String> nodeNames = new ArrayList<String>();
+        final List<Exception> prepareExceptions = new ArrayList<Exception>();
+        final List<Exception> runExceptions = new ArrayList<Exception>();
+		for (int i=0; i<runWorkerThreads.length; i++) {
+			runResults.add(runWorkerThreads[i].result);
+			prepResults.add(prepareWorkerThreads[i].result);
+			nodeNames.add(runWorkerThreads[i].iNodeAddr);
+			prepareExceptions.add(prepareWorkerThreads[i].exception);
+			runExceptions.add(runWorkerThreads[i].exception);
+		}
 			
-			try {
-				// remote deploy
-				System.out.println("deploying on node " + node.getINetAdress() + " called.");
-				DeploymentHelper.getInstance().deployRemote(node.getINetAdress(), lfile, md5cs, shell, true);
-				mask.add(true);
-			} catch (Exception e) {
-				System.err.println("ErrorMsg:" + e.getMessage());
-				MessageDialog.openError(editor.getSite().getShell(), "Clickwatch Error", "ErrorMsg:" + e.getMessage());			
-				mask.add(false);
-			}
+        // show results in treeview
+		showResults(prepResults, runResults, nodeNames);
+		
+		// show exceptions in popup window
+		showExceptions(nodeNames, prepareExceptions, runExceptions);
+	}
+
+	/**
+	 * Update results in resultview 
+	 */
+	private void showResults(List<String> prepResults, List<String> runResults, List<String> nodeNames) {
+
+		// create xml from results string
+		StringBuffer xmlResults = new StringBuffer();
+		xmlResults.append("<network>");
+		for (int i=0; i<prepResults.size(); i++) {
+			xmlResults.append("<node id='" + nodeNames.get(i) + "'>");
+			xmlResults.append("<prepare>");
+			xmlResults.append(prepResults.get(i));
+			xmlResults.append("</prepare>");
+			xmlResults.append("<run>");
+			xmlResults.append(runResults.get(i));
+			xmlResults.append("</run>");
+			xmlResults.append("</node>");
 		}
+		xmlResults.append("</network>");
+		
+		System.out.println("XMl results to display: " + xmlResults.toString());
+		
+		EObject result = XmlUtil.deserializeXml(xmlResults.toString());
 
-        node_it = node_lst.iterator();
-        Iterator<Boolean> mask_it = mask.iterator();
-		//
-        // Step 2: start router conf
-		while (node_it.hasNext()) {
-			Node node = node_it.next();
-			Boolean status = mask_it.next();
-
-			// remote deploy
-			if (status) {
-				System.out.println("starting on node " + node.getINetAdress() + " called.");
-				try {
-					String remoteLog = DeploymentHelper.getInstance().startRemote(node.getINetAdress(), shell, true);
-					if (show_log) {
-						MessageDialog.openInformation(editor.getSite().getShell(), "Clickwatch Log Info", "Remote Log: " + remoteLog);			
-					}
-				} catch (Exception e) {
-					System.err.println("ErrorMsg:" + e.getMessage());
-					MessageDialog.openError(editor.getSite().getShell(), "Clickwatch Error", "ErrorMsg:" + e.getMessage());			
+		for(IViewReference viewRef: editor.getEditorSite().getPage().getViewReferences()) {
+			IViewPart view = viewRef.getView(false);
+			if (view instanceof ResultView) {
+				((ResultView)view).setInput(result);
+				if (currentResult != null) {
+					EcoreUtil.delete(currentResult, true);			
 				}
-			} else {
-				// do not start failed nodes
+				currentResult = result;
 			}
 		}
 	}
 
+	/**
+	 * At the end display the statistics about failed execs
+	 */
+	private void showExceptions(List<String> nodeNames, List<Exception> prepareExceptions, List<Exception> runExceptions) {
+		StringBuffer txtExc = new StringBuffer();
+		for (int i=0; i<prepareExceptions.size(); i++) {
+			txtExc.append("Node ").append(nodeNames.get(i)).append(" -> ").append(" Prepare: ");
+			if (prepareExceptions.get(i) != null) {
+				txtExc.append(prepareExceptions.get(i).getMessage());
+			} else {
+				txtExc.append("OK");
+			}
+			txtExc.append(", Run: ");
+			if (runExceptions.get(i) != null) {
+				txtExc.append(runExceptions.get(i).getMessage());
+			} else {
+				txtExc.append("OK");
+			}
+			txtExc.append("\n");
+		}
+		
+		MessageDialog.openError(editor.getSite().getShell(), "Result stats", txtExc.toString());		
+	}
+	
 	/**
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
