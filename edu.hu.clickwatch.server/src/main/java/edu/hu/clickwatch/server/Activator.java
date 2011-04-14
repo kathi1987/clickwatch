f12d2bfed9daf8ce83a60fdef884f5b965a37451
Michael Frey <frey@informatik.hu-berlin.de>
2011-04-14 17:42:41 +0200

    Setup maven for the clickwatch project, generated pom.xml files for projects, added .gitignore file, added server project

---
+++ edu.hu.clickwatch.server/src/main/java/edu/hu/clickwatch/server/Activator.java
@@ -0,0 +1,33 @@
+package edu.hu.clickwatch.server;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+import edu.hu.clickwatch.model.Node;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		Node node;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
