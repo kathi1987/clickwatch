5c629774d8fd06dea034b992ea8dbf2091c2834c
Michael Frey <frey@informatik.hu-berlin.de>
2011-07-01 12:47:17 +0200

    Renamed projects from 'edu.hu....' to 'de.hub...' in module server

---
+++ server/de.hub.clickwatch.xml.tests/src/test/java/edu/hu/clickwatch/xml/tests/CdoTypeTest.java
@@ -0,0 +1,93 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package edu.hu.clickwatch.xml.tests;
+
+import edu.hu.clickwatch.xml.CdoType;
+import edu.hu.clickwatch.xml.ConfigurationFactory;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Cdo Type</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class CdoTypeTest extends TestCase {
+
+	/**
+	 * The fixture for this Cdo Type test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CdoType fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(CdoTypeTest.class);
+	}
+
+	/**
+	 * Constructs a new Cdo Type test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CdoTypeTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this Cdo Type test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(CdoType fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this Cdo Type test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected CdoType getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(ConfigurationFactory.eINSTANCE.createCdoType());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //CdoTypeTest
